a8bcb46b6bf544e8a215f4660632eac6f6374c25
SJlydog <makhanov188@gmail.com>
2017-07-12 11:16:33 +0300

    commit

--- audit/js/common.js
+++ audit/js/common.js
@@ -84,26 +84,35 @@ $(function() {
 		responsive:{
 			0:{
 				items:1,
-				// nav:true
+				// nav:true,
+				// loop:true,
 			},
 			480:{
 				items:2,
-				nav:true
+				// nav:true,
+				// loop:true,
 			},
 			768:{
 				items:3,
-				// nav:true
+				// loop:true,
+				// nav:true,
 			},
 			1000:{
 				items:4,
 				// nav:true,
-				loop:false
+				// loop:true,
 			},
 			1200:{
 				items:4,
-				// nav:true,
-				loop:false
-			}
+				nav:true,
+				// loop:true,
+			},
+			// 1900:{
+			// 	items:5,
+			// 	// nav:true,
+			// 	// loop:true,
+			// }
+
 		}
 	});
 
